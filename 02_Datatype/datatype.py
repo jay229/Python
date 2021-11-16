7cb1af38be5f7c383acfd4867dcfce033a9b3fd9
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2021-11-16 17:51:38 +0530

    From today have started learning python

---
+++ 02_Datatype/datatype.py
@@ -0,0 +1,20 @@
+# int data type
+# a=10
+# b=10
+# print(type(a))
+# print( type(b))
+# print(id(a))
+# print(id(b))
+# b=20
+# print(id(b))
+
+
+# float data type
+a=10.5
+b=10.5
+print(type(a))
+print( type(b))
+print(id(a))
+print(id(b))
+b=20.5
+print(id(b))
