bdf3a6979bc66481aa50b6a351e4a3abb764c92c
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2021-11-18 19:15:28 +0530

    data type is completed

--- 02_Datatype/datatype.py
+++ 02_Datatype/datatype.py
@@ -8,13 +8,137 @@
 # b=20
 # print(id(b))
 
+# int Binary form
+# a=0b10
+# b=0b10
+# print(id(a))
+# print(id(b))
+# b=0b11
+# print(id(b))
+
+
+# int Octal form
+# a=0o766
+# b=0o466
+# print(id(a))
+# print(type(a))
+# print(type(b))
+# print(id(b))
+# b=0o76
+# print(id(b))
+
+# int hexadecimal form
+# a=0x766
+# b=0x466
+# print(id(a))
+# print(type(a))
+# print(type(b))
+# print(id(b))
+# b=0x76
+# print(id(b))
+
+
+# Base Conversion
+# bin()
+# Decimal to Binary
+# a=205
+# print(bin(a))
+
+# Octal to Binary
+# a=0o17
+# print(bin(a))
+
+# Hexa decimal to Binary
+# a=0x1a
+# print(bin(a))
+
+
+# oct()
+# Decimal to octal
+# a=205
+# print(oct(a))
+
+# Binary to octal
+# a=0b11
+# print(oct(a))
+
+# Hexa decimal to octal
+# a=0x1a
+# print(oct(a))
+
+
+# hex()
+# Decimal to hexa Decimal
+# a=205
+# print(hex(a))
+
+# Binary to hexa Decimal
+# a=0b11
+# print(hex(a))
+
+# Octal to Hexa decimal 
+# a=0b1
+# print(hex(a))
+
+
+
 
 # float data type
-a=10.5
-b=10.5
-print(type(a))
-print( type(b))
-print(id(a))
-print(id(b))
-b=20.5
-print(id(b))
+# a=10.5
+# b=10.5
+# print(type(a))
+# print( type(b))
+# print(id(a))
+# print(id(b))
+# b=20.5
+# print(id(b))
+
+# Complex data type
+# a=5+4j
+# a=5.5+4j
+# a=5.5+4.5j
+# a=5+4.5j
+
+
+# a=0b11+4.5j
+# a=0b11+0b11j It will show an error
+# print(type(a))
+
+# Arithmetic operation on complex data type
+# a=5+3j
+# b=4+2j
+# print(a+b)
+# print(a-b)
+# print(a*b)
+
+# print(a.real)
+# print(a.imag)
+
+# bool data type
+# True and false
+# a=10
+# b=20
+# c=a>b
+# print(c)
+# c=a<b
+# print(c)
+
+# print(True+True)
+# print(False+True)
+
+# String data type (str)
+# a="Mritunjay"
+# print(a)
+# print(a[0])
+a='''Mritunjay
+Kumar 
+Tiwari \t '''
+# print(a)
+# print(a[0])
+# print(a[-1])
+# print(a[0:40])
+# print(a[0:])
+# print(a[:2])
+# print(a[:])
+# print(a*3)
+print(len(a))
