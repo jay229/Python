c8edc64b47b8f164545973501a559013e27d8c8f
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2021-11-21 19:09:17 +0530

    Type casting is completed

---
+++ 03_Type Casting/typeCasting.py
@@ -0,0 +1,46 @@
+# int()
+# a=10.5
+# a=10+5j It will show error
+# a="10"
+# a="ten" It will show error
+# a=True
+# a=False
+# print(int(a))
+
+# float()
+# a=10
+# a=10+3j It will show error
+# a=True
+# a=False
+# a="10"
+# a="Ten" It will show error
+# print(float(a))
+
+# complex()
+# form 1: complex(x)
+# print(complex(10))
+# print(complex(True))
+# print(complex(False))
+# print(complex("10"))
+# print(complex("10.5"))
+# print(complex("five")) It will show error
+
+# form 1: complex(x,y)
+# print(complex(10,5))
+# print(complex(True,True))
+# print(complex(False,True))
+# print(complex(True,False))
+# print(complex("10","5"))It will show error
+# print(complex(5,"5"))It will show error
+# print(complex(5.5,5))
+
+# bool()
+# print(bool(1))
+# print(bool(0))
+# print(bool("True"))
+# print(bool("False"))
+# print(bool(""))
+
+# str()
+print(str(1))
+
